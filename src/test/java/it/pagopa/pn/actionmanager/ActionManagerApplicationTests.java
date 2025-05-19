bfe49417b93df47c42d1945d2e039829fd424482
vincenzo-racca-pa <vincenzo.racca22@gmail.com>
2025-05-19 15:21:00 +0200

    add a java test

---
+++ src/test/java/it/pagopa/pn/actionmanager/ActionManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package it.pagopa.pn.actionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
