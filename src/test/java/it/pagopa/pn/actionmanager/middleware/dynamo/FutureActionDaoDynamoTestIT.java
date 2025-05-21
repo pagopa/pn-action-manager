61946d18ef9221f354902aa31c400e084616a0f4
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2025-05-21 17:51:35 +0200

    [PN-14957] fix test IT

--- src/test/java/it/pagopa/pn/actionmanager/middleware/dynamo/FutureActionDaoDynamoTestIT.java
+++ src/test/java/it/pagopa/pn/actionmanager/middleware/dynamo/FutureActionDaoDynamoTestIT.java
@@ -4,8 +4,9 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import it.pagopa.pn.actionmanager.LocalStackTestConfig;
+import it.pagopa.pn.actionmanager.exceptions.PnNotFoundException;
 import it.pagopa.pn.actionmanager.middleware.dao.actiondao.FutureActionDao;
-import it.pagopa.pn.actionmanager.middleware.dao.actiondao.dynamo.ActionDaoDynamo;
+import it.pagopa.pn.actionmanager.middleware.dao.actiondao.dynamo.FutureActionDaoDynamo;
 import it.pagopa.pn.commons.abstractions.impl.MiddlewareTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -37,7 +38,7 @@ class FutureActionDaoDynamoTestIT {
         String actionId = "Test_unscheduleActionFailSilent_actionId";
 
         // Ottieni il logger di Logback
-        Logger fooLogger = LoggerFactory.getLogger(ActionDaoDynamo.class);
+        Logger fooLogger = LoggerFactory.getLogger(FutureActionDaoDynamo.class);
 
         // Crea e avvia un ListAppender
         ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
@@ -47,18 +48,17 @@ class FutureActionDaoDynamoTestIT {
         ((ch.qos.logback.classic.Logger) fooLogger).addAppender(listAppender);
 
         // Verifica che il metodo non lanci eccezioni
-        Assertions.assertDoesNotThrow(() ->
-                futureActionDao.unscheduleAction(timeSlot, actionId)
+        Assertions.assertThrows(
+                PnNotFoundException.class,
+                () -> futureActionDao.unscheduleAction(timeSlot, actionId)
         );
 
         // Recupera i log registrati
         List<ILoggingEvent> logsList = listAppender.list;
 
         // Verifica che il messaggio di log sia corretto
-        Assertions.assertEquals(
-                String.format("Action not found for timeSlot=%s and actionId=%s", timeSlot, actionId),
-                logsList.get(0).getFormattedMessage()
-        );
+        String expectedMessage = String.format("Action not found for timeSlot=%s and actionId=%s", timeSlot, actionId);
+        Assertions.assertEquals(expectedMessage, logsList.get(0).getFormattedMessage());
         Assertions.assertEquals(Level.ERROR, logsList.get(0).getLevel());
     }
 
