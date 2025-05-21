94e80b59bce8ee639282f99169a0b74560b5c1ed
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-05-21 13:15:32 +0200

    fix: test configuration

--- src/main/java/it/pagopa/pn/actionmanager/config/PnActionManagerConfigs.java
+++ src/main/java/it/pagopa/pn/actionmanager/config/PnActionManagerConfigs.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import javax.annotation.PostConstruct;
-import java.time.Duration;
-import java.time.Instant;
+import jakarta.annotation.PostConstruct;
 @Configuration
 @ConfigurationProperties(prefix = "pn.action-manager")
 @Data
