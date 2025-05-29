5010568a8cfb6d481bce4416f27ecd5a788d7dda
simone.rondinella <simone.rondinella@pagopa.it>
2025-05-29 15:09:09 +0200

    rimozione system property

--- src/main/java/it/pagopa/pn/actionmanager/ActionManagerApplication.java
+++ src/main/java/it/pagopa/pn/actionmanager/ActionManagerApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActionManagerApplication {
 
     public static void main(String[] args) {
-        System.setProperty("io.netty.noUnsafe", "true");
-
         SpringApplication.run(ActionManagerApplication.class, args);
     }
 }
