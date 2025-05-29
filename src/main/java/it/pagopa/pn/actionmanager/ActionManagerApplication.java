aa2b94bcd79eba95573669074fc1d11cd2341e03
simone.rondinella <simone.rondinella@pagopa.it>
2025-05-29 12:36:37 +0200

    Modifiche utili per netty

--- src/main/java/it/pagopa/pn/actionmanager/ActionManagerApplication.java
+++ src/main/java/it/pagopa/pn/actionmanager/ActionManagerApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActionManagerApplication {
 
-
     public static void main(String[] args) {
+        System.setProperty("io.netty.noUnsafe", "true");
+
         SpringApplication.run(ActionManagerApplication.class, args);
     }
 }
