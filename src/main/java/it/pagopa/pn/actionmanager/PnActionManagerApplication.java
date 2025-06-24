274019fb827e20c511a85294399676be61122add
simone.rondinella <simone.rondinella@pagopa.it>
2025-06-24 11:52:03 +0200

    Prova log

--- src/main/java/it/pagopa/pn/actionmanager/PnActionManagerApplication.java
+++ src/main/java/it/pagopa/pn/actionmanager/PnActionManagerApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static it.pagopa.pn.actionmanager.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
 
+import java.io.InputStream;
+
 @SpringBootApplication
 public class PnActionManagerApplication {
 
 
     public static void main(String[] args) {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("logback-base.xml");
+        System.out.println("Logback config found: " + (is != null));
+
         buildSpringApplicationWithListener().run(args);
     }
 
