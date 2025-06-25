26d0f06c83d9d2ce07a07b8df1aac904a5e16535
Andrea Bertucci <andrea.bertucci@pagopa.it>
2025-06-25 13:55:23 +0200

    feat: added configuration for logging configuration

--- src/main/java/it/pagopa/pn/actionmanager/PnActionManagerApplication.java
+++ src/main/java/it/pagopa/pn/actionmanager/PnActionManagerApplication.java
@@ -1,15 +1,33 @@
 package it.pagopa.pn.actionmanager;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.LoggerFactory;
+
+
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static it.pagopa.pn.actionmanager.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
+import java.net.URL;
 
+import static it.pagopa.pn.actionmanager.utils.SpringApplicationUtils.buildSpringApplicationWithListener;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import org.slf4j.LoggerFactory;
+import java.net.URL;
 @SpringBootApplication
 public class PnActionManagerApplication {
 
 
     public static void main(String[] args) {
+
+
+        URL url = PnActionManagerApplication.class.getClassLoader().getResource("logback-base.xml");
+        System.out.println("!!!!!**************************************************************logback-base.xml: " + url);
+
         buildSpringApplicationWithListener().run(args);
+
     }
 
 }
